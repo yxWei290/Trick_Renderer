6f9dd3f71d51a7cf68775580eec642a67666cd70
yxWei290 <65759488+yxWei290@users.noreply.github.com>
2022-08-30 19:49:28 +0800

    Update model.h

--- model.h
+++ model.h
@@ -2,23 +2,23 @@
 #define __MODEL_H__
 
 #include <vector>
-#include "geometry.h"
+#include <glm/glm.hpp>
 
 class Model {
 private:
-	std::vector<Vec3f> verts_;
+	std::vector<glm::vec3> verts_;
 	std::vector<int> facet_vrt;
 	std::vector<int> facet_tex;  // per-triangle indices in the above arrays
 	std::vector<int> facet_nrm;
-	std::vector<Vec2f> tex_coord;
+	std::vector<glm::vec2> tex_coord;
 public:
 	Model(const char *filename);
 	~Model();
 	int nverts();
 	int nfaces();
-	Vec3f vert(int nface, int i);
+	glm::vec3 vert(int nface, int i);
 
-	Vec2f uv(const int nface, const int nthvert) const;
+	glm::vec2 uv(const int nface, const int nthvert) const;
 };
 
 #endif //__MODEL_H__
