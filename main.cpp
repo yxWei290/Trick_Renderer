bd80d68756ce3babcc95e2bf9c886c3db1d12726
yxWei290 <65759488+yxWei290@users.noreply.github.com>
2022-09-08 18:23:55 +0800

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -24,11 +24,12 @@ glm::vec4 norm(const glm::vec4 v)
 // 求顶点法线及强度，插值给面内的片段
 struct GouraudShader : public IShader {
     glm::vec3 varying_intensity; // 顶点着色器写，片段着色器读。三角形三个顶点的光照强度值
+    glm::vec2 varying_uv[3];     // 三角形三个顶点的uv坐标
 
     virtual glm::vec3 vertex(int iface, int nthvert) 
     {
+        varying_uv[nthvert] = model->uv(iface, nthvert);
         glm::vec3 gl_vertex = model->vert(iface, nthvert);
-        //glm::vec4 test = /**/Viewport * Projection * ModelView * glm::vec4(gl_vertex, 1.0f);
         gl_vertex = Viewport *norm( Projection * ModelView * glm::vec4(gl_vertex, 1.0f));
         varying_intensity[nthvert] = std::max(0.0f, glm::dot(model->normal(iface, nthvert), light_dir));
         return gl_vertex;
@@ -38,7 +39,9 @@ struct GouraudShader : public IShader {
     virtual bool fragment(glm::vec3 bar, TGAColor& color)
     {
         float intensity = glm::dot(varying_intensity, bar);
-        color = TGAColor(255, 255, 255) * intensity; 
+        glm::vec2 uv = varying_uv[0] * bar.x + varying_uv[1] * bar.y + varying_uv[2] * bar.z;
+        color = model->diffuse().get(uv.x*model->diffuse().get_width(), uv.y*model->diffuse().get_height())* intensity;
+
         return false;
     }
 };
@@ -70,19 +73,16 @@ int main(int argc, char** argv)
     model = new Model("obj/african_head.obj");
     lookat(eye, center, up);
     viewport(width , height);
-    projection(glm::radians(45.0f), 1.0f / 1.0f, 0.1f, 100.0f);
+    projection(glm::radians(45.0f), 1.0f / 1.0f, 0.1f, 5.0f);
     light_dir = glm::normalize(light_dir);
 
     TGAImage image(width, height, TGAImage::RGB);
-    //TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
     float* zbuffer = new float[width * height];
     for (int i = 0; i < width * height; i++) {
         zbuffer[i] = std::numeric_limits<float>::max();
     }
 
-    GouraudShader shader;   //在这里更换着色器
-    //PhongShader shader;
-    
+    GouraudShader shader;
     for (int i = 0; i < model->nfaces(); i++) 
     {
         glm::vec3 screen_coords[3];
@@ -93,7 +93,7 @@ int main(int argc, char** argv)
 
     image.flip_vertically(); // to place the origin in the bottom left corner of the image
     image.write_tga_file("output/output.tga");
-
+    
     delete model;
     return 0;
 }
