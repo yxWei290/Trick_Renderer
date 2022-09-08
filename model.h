b40162c051ae5eb49aecc96f396532bfae04a100
yxWei290 <65759488+yxWei290@users.noreply.github.com>
2022-09-08 18:24:27 +0800

    Update model.h

--- model.h
+++ model.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <glm/glm.hpp>
+#include "tgaimage.h"
 
 class Model {
 private:
@@ -12,6 +13,8 @@ private:
 	std::vector<int> facet_vrt;
 	std::vector<int> facet_tex;  // per-triangle indices in the above arrays
 	std::vector<int> facet_nrm;
+	TGAImage diffusemap;         // diffuse color texture
+	void load_texture(const std::string filename, const std::string suffix, TGAImage& img);	//在对象初始化过程中自动加载纹理，包括材质、漫反射等
 	
 public:
 	Model(const char *filename);
@@ -21,6 +24,7 @@ public:
 	glm::vec3 vert(int nface, int i);
 	glm::vec3 normal(const int iface, const int nthvert) const;
 	glm::vec2 uv(const int nface, const int nthvert) const;
+	TGAImage& diffuse()  { return diffusemap;  }
 };
 
 #endif //__MODEL_H__
