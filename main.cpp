4ca191ce409dda246449c237067094614bf739dc
yxWei290 <65759488+yxWei290@users.noreply.github.com>
2022-09-14 17:06:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -74,26 +74,53 @@ struct Shader : public IShader {
     glm::mat4 uniform_M;   //  Projection*ModelView
     glm::mat4 uniform_MIT; // (Projection*ModelView)的逆转置
     glm::vec3 gl_vertex[3];// 存储世界坐标
+    glm::vec3 scr_vertex[3];// 存储屏幕空间坐标
+    glm::vec3 gl_normal[3];// 存储三个顶点的法线
 
     virtual glm::vec3 vertex(int iface, int nthvert) 
     {
         varying_uv[nthvert] = model->uv(iface, nthvert);
         gl_vertex[nthvert] = model->vert(iface, nthvert);
-        return norm(Viewport * Projection * ModelView * glm::vec4(gl_vertex[nthvert], 1.0f)); // transform it to screen coordinates
+        gl_normal[nthvert] = model->normal(iface, nthvert);
+        scr_vertex[nthvert] = norm(Viewport * Projection * ModelView * glm::vec4(gl_vertex[nthvert], 1.0f));
+        return scr_vertex[nthvert]; // transform it to screen coordinates
     }
 
     virtual bool fragment(glm::vec3 bar, TGAColor& color) {
-        glm::vec2 uv = varying_uv[0] * bar.x + varying_uv[1] * bar.y + varying_uv[2] * bar.z;
-        glm::vec3 n = glm::normalize(model->normal(uv));
+        // 经过透视矫正插值计算当前法线
+        float Zt = 1 / (bar.x / scr_vertex[0].z + bar.y / scr_vertex[1].z + bar.z / scr_vertex[2].z);   //该像素经过矫正的Z值
+        glm::vec3 nt = Zt * (bar.x * gl_normal[0] / scr_vertex[0].z + bar.y * gl_normal[1] / scr_vertex[1].z + bar.z * gl_normal[2] / scr_vertex[2].z);
+        glm::vec2 uv = Zt * (varying_uv[0] * bar.x / scr_vertex[0].z + varying_uv[1] * bar.y / scr_vertex[1].z + varying_uv[2] * bar.z / scr_vertex[2].z);
+        glm::vec3 n = glm::normalize(nt);
         glm::vec3 l = light_dir;
         glm::vec3 r = glm::normalize(n * glm::dot(n, l) * 2.0f - l);   // 反射向量
 
         glm::vec3 frag_coord = gl_vertex[0] * bar.x + gl_vertex[1] * bar.y + gl_vertex[2] * bar.z;  //计算当前像素的世界坐标
         float spec = pow(std::max(glm::dot(glm::normalize(eye - frag_coord), r), 0.0f), model->specular(uv));
+
+        //计算TBN并乘以切线空间下的法线坐标
+        glm::vec3 T, B, N;
+        N = n;
+        glm::vec3 E1 = gl_vertex[1] - gl_vertex[0];
+        glm::vec3 E2 = gl_vertex[2] - gl_vertex[0];
+        float delta_u1 = (varying_uv[1] - varying_uv[0]).x;
+        float delta_u2 = (varying_uv[2] - varying_uv[0]).x;
+        float delta_v1 = (varying_uv[1] - varying_uv[0]).y;
+        float delta_v2 = (varying_uv[2] - varying_uv[0]).y;
+        T = (delta_v1 * E2 - delta_v2 * E1) / (delta_v1 * delta_u2 - delta_v2 * delta_u1);
+        B = (-delta_u1 * E2 + delta_u2 * E1) / (delta_v1 * delta_u2 - delta_v2 * delta_u1);
+
+        T = glm::normalize(T - glm::dot(T, N) * N);     //施密特正交化
+        B = glm::normalize(B - glm::dot(B, N) * N - glm::dot(B, T) * T);
+
+        glm::mat3 TBN(T.x, T.y, T.z, B.x, B.y, B.z, N.x, N.y, N.z);
+        
+        n = glm::normalize(TBN * model->normal(uv));
+
         float diff = std::max(0.0f, glm::dot(n, l));
         TGAColor c = model->diffuse(uv);
         color = c;
-        for (int i = 0; i < 3; i++) color[i] = std::min<float>(5 + c[i] * (diff + spec), 255);   //5代表环境光，系数自由决定
+        for (int i = 0; i < 3; i++) color[i] = std::min<float>(0 + c[i] * (diff /* + spec*/), 255);   //5代表环境光，系数自由决定
         return false;
     }
 };
